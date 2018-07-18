5fd48ae74cf59909bb5479eeb94f5118dd3f9d2e
Grace Gong <gracegong1@gmail.com>
2018-07-18 10:58:44 -0700

    removed firebase, implemented coredata

--- Podfile
+++ Podfile
@@ -3,14 +3,9 @@ platform :ios, '9.0'
 
 target 'sced app' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
+  # use_frameworks!
 
   # Pods for sced app
 
-pod 'Firebase/Core'
-pod 'Firebase/Auth'
-pod 'FirebaseUI/Auth'
-pod 'Firebase/Database'
-pod 'Firebase/Storage'
 
 end
--- sced app/Helpers/CoreDataHelper.swift
+++ sced app/Helpers/CoreDataHelper.swift
@@ -11,5 +11,47 @@ import CoreData
 import UIKit
 
 struct CoreDataHelper {
-  
+    static let context: NSManagedObjectContext = {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
+        }
+        
+        let persistentContainer = appDelegate.persistentContainer
+        let context = persistentContainer.viewContext
+        
+        return context
+    }()
+    
+    static func newPeriod() -> Period {
+        let period = NSEntityDescription.insertNewObject(forEntityName: "Period", into: context) as! Period
+        
+        return period
+    }
+    
+    static func savePeriod() {
+        do {
+            try context.save()
+        } catch let error {
+            print("could not save \(error.localizedDescription)")
+        }
+    }
+    
+    static func delete(period: Period) {
+        context.delete(period)
+        
+        savePeriod()
+    }
+    
+    static func retrievePeriods() -> [Period] {
+        do {
+            let fetchRequest = NSFetchRequest<Period>(entityName: "Period")
+            let results = try context.fetch(fetchRequest)
+
+            return results
+            
+        } catch let error {
+            print("could not fetch \(error.localizedDescription)")
+            return []
+        }
+    }
 }
